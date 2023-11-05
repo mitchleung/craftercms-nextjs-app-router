8c0bcfb005db55212af5507742213e37a863df35
Mitch Leung <mitchleung.com@gmail.com>
2023-11-05 22:21:13 +1300

    Fix eslint config on next.config.js

--- app/next.config.js
+++ app/next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: [
+      'localhost'
+    ]
+  },
+  reactStrictMode: true,
+  transpilePackages: ['@craftercms/experience-builder', '@craftercms/studio-ui']
+}
 
 module.exports = nextConfig
